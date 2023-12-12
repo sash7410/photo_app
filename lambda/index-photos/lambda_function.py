b708bc40476271faf9769ec9028a8501c2a29d86
sashank <sashankrm11@gmail.com>
2023-12-12 00:54:11 -0500

    updating lambda functions

--- lambda/index-photos/lambda_function.py
+++ lambda/index-photos/lambda_function.py
@@ -2,84 +2,71 @@ import boto3
 import json
 import requests
 import base64
+import logging
+
+def initialize_clients():
+    return boto3.client('s3'), boto3.client('rekognition')
+
+def fetch_image_from_s3(s3_client, bucket_name, object_key):
+    image_response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    return base64.b64decode(image_response['Body'].read())
 
 
-def update_elasticsearch_index(index_name, document):
-    es.index(index=index_name, body=document)
+def analyze_image_with_rekognition(rekognition_client, image_content):
+    rekognition_response = rekognition_client.detect_labels(
+        Image={'Bytes': image_content},
+        MinConfidence=80
+    )
+    return [label['Name'] for label in rekognition_response['Labels']]
+
+def get_s3_object_metadata(s3_client, bucket_name, object_key):
+    return s3_client.head_object(Bucket=bucket_name, Key=object_key)
+
+def update_es_index(elasticsearch_url, index_name, document_id, document):
+    url = f"{elasticsearch_url}/{index_name}/_doc/{document_id}"
+    response = requests.post(url, data=json.dumps(document), auth=("root", "Root@123"), headers={"Content-Type": "application/json"})
+    logging.info(f"ES Update Response: {response.status_code}, Content: {response.content.decode('utf-8')}")
 
 def lambda_handler(event, context):
-    # Retrieve information about the uploaded image from the S3 event
-    s3_bucket = event['Records'][0]['s3']['bucket']['name']
-    s3_object_key = event['Records'][0]['s3']['object']['key']
-    # Initialize S3 client
-    s3 = boto3.client('s3')
-    # Initialize Rekognition client
-    rekognition = boto3.client('rekognition')
-    # Extract S3 object metadata
-    response = s3.get_object(Bucket=s3_bucket, Key=s3_object_key)
-    base64_encoded_image_content = response['Body'].read()
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Starting Lambda Handler")
 
-    # Decode the base64 string
-    decoded_image_content = base64.b64decode(base64_encoded_image_content)
-    # Call detectLabels method
-    response = rekognition.detect_labels(
-        Image={'Bytes': decoded_image_content},
-        MinConfidence=80  # Minimum confidence level for detected labels (adjust as needed)
-    )
-    
-    # Process the response - extract and handle labels
-    labels = [label['Name'] for label in response['Labels']]
-    print(labels)
-    # You can further process or store these labels as needed (e.g., in a database or another AWS service)
+    # Extract S3 bucket and object information from the event
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+    logging.info(f"Processing S3 Object: {object_key} in Bucket: {bucket_name}")
+
+    # Initialize AWS clients
+    s3_client, rekognition_client = initialize_clients()
+
+    # Fetching and analyzing the image
+    image_content = fetch_image_from_s3(s3_client, bucket_name, object_key)
+    labels_detected = analyze_image_with_rekognition(rekognition_client, image_content)
+    logging.info(f"Detected Labels: {labels_detected}")
 
     # Retrieve S3 object metadata
     try:
-        response = s3.head_object(Bucket=s3_bucket, Key=s3_object_key)
-        print(response)
-        created_timestamp = response['LastModified']
-        created_timestamp = created_timestamp.isoformat()
-        # Check if x-amz-meta-customLabels field exists in metadata
-        # If x-amz-meta-customLabels field exists, create a JSON array
+        object_metadata = get_s3_object_metadata(s3_client, bucket_name, object_key)
+        creation_timestamp = object_metadata['LastModified'].isoformat()
 
-        if 'customlabels' in response['Metadata']:
-            # labels_array = response['Metadata']['customlabels']
-            # Process or utilize labels_array as needed
-            custom_labels_string = response['Metadata']['customlabels']
-            custom_labels_array = custom_labels_string.split(',')
-            labels.extend(custom_labels_array)
-            json_object = {
-            "objectKey": s3_object_key,
-            "bucket": s3_bucket,
-            "createdTimestamp": created_timestamp,
-            "labels": labels
-            }
-        else:
-            print("No custom labels metadata found.")
-            json_object = {
-            "objectKey": s3_object_key,
-            "bucket": s3_bucket,
-            "createdTimestamp": created_timestamp,
-            "labels": labels
-            }
-        auth = ("root", "Root@123")
-        index_name = "photosidx"
-        elasticsearch_url = "https://search-photos-5l4vvfb77o22onqn6kixmrg2gi.us-west-2.es.amazonaws.com"
-        url = f"{elasticsearch_url}/{index_name}/_doc/{s3_object_key}"
-        json_object = json.dumps(json_object)
-        print(json_object)
-        response = requests.post(url, data=json_object, auth=auth, headers={"Content-Type": "application/json"})
-        
-        print(response)
-        print("Response status code:", response.status_code)
-        print("Response headers:", response.headers)
-        print("Response content:", response.content.decode('utf-8'))
-    except Exception as e:
-        print("Error retrieving metadata:", e)
-        return {
-            'statusCode': 500,
-            'body': "Error retrieving metadata."
+        if 'customlabels' in object_metadata['Metadata']:
+            custom_labels = object_metadata['Metadata']['customlabels'].split(',')
+            labels_detected.extend(custom_labels)
+
+        es_document = {
+            "objectKey": object_key,
+            "bucket": bucket_name,
+            "createdTimestamp": creation_timestamp,
+            "labels": labels_detected
         }
-    return {
-        'statusCode': 200,
-        'body': labels  # Sending labels as the response
-    }
+
+        # Update Elasticsearch index
+        es_index_name = "photosidx"
+        es_url = "https://search-photos-5l4vvfb77o22onqn6kixmrg2gi.us-west-2.es.amazonaws.com"
+        update_es_index(es_url, es_index_name, object_key, es_document)
+
+    except Exception as error:
+        logging.error(f"Error retrieving metadata: {error}")
+        return {'statusCode': 500, 'body': "Error retrieving metadata."}
+
+    return {'statusCode': 200, 'body': labels_detected}
--- lambda/lf2/lambda_function.py
+++ lambda/lf2/lambda_function.py
@@ -3,99 +3,97 @@ import json
 import logging
 import requests
 
+# Set up logging
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
-headers = {"Content-Type": "application/json"}
-host = "https://search-photos-5l4vvfb77o22onqn6kixmrg2gi.us-west-2.es.amazonaws.com"
-region = 'us-west-2'
-lex_v2 = boto3.client('lexv2-runtime', region_name=region)
+# Constants for the Elasticsearch service and Lex V2 client
+HEADERS = {"Content-Type": "application/json"}
+ES_HOST = "https://search-photos-5l4vvfb77o22onqn6kixmrg2gi.us-west-2.es.amazonaws.com"
+REGION = 'us-west-2'
+lex_client = boto3.client('lexv2-runtime', region_name=REGION)
+
 
 def lambda_handler(event, context):
-
-    print("EVENT --- {}".format(json.dumps(event)))
-    q1 = event["queryStringParameters"]["q"]
-    print('nowww')
-    print(q1)
-    print('now2222')
-    if(q1 == "searchAudio"):
-        q1 = convert_speechtotext()
-
-    print("q1:", q1)
-    labels = get_labels(q1)
-    print("labels", labels)
-    if len(labels) == 0:
-        return
-    else:
-        img_paths = get_photo_path(labels)
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'imagePaths': img_paths,
-            'userQuery': q1,
-            'labels': labels,
-        }),
-        'headers': {
-            'Access-Control-Allow-Origin': '*'
-        },
-        "isBase64Encoded": False
-    }
+    logger.info(f"Received event: {json.dumps(event)}")
+    user_query = get_query_from_event(event)
     
+    if is_search_audio_request(user_query):
+        user_query = convert_speech_to_text()  
+    
+    
+    labels = extract_labels_from_lex(user_query)
+    
+    if not labels:
+        logger.info("No labels extracted from query.")
+        return build_response(200, {"message": "No labels found"})
+    
+    image_paths = fetch_image_paths_from_elasticsearch(labels)
+    return build_response(200, {
+        'imagePaths': image_paths,
+        'userQuery': user_query,
+        'labels': labels,
+    })
 
-def get_labels(query):
-    # Make sure to replace 'yourBotId', 'yourBotAliasId', and 'yourLocaleId' with your actual bot details
-    response = lex_v2.recognize_text(
+def get_query_from_event(event):
+    return event.get("queryStringParameters", {}).get("q", "")
+
+def is_search_audio_request(query):
+    return query.lower() == "searchaudio"
+
+def convert_speech_to_text():
+    # Placeholder for speech to text conversion logic
+    return "converted text"
+
+def extract_labels_from_lex(query):
+    response = lex_client.recognize_text(
         botId='HWYJTUQXRC',
         botAliasId='TSTALIASID',
         localeId='en_US',
-        sessionId='string',  # Replace 'string' with a unique identifier for the user's session
+        sessionId='uniqueSessionId',  # Replace with a unique identifier for the user's session
         text=query
     )
-    print("lex-response", response)
-
-    labels = []
-    # The structure of the response for Lex V2 is different from V1.
-    # Lex V2 uses 'interpretedValues' key under 'slots' to get the slot values
-    if 'sessionState' in response and 'intent' in response['sessionState']:
-        slots = response['sessionState']['intent'].get('slots', {})
-        for key, slot_detail in slots.items():
-            # Check if slot has been filled
-            if slot_detail and 'value' in slot_detail and 'interpretedValue' in slot_detail['value']:
-                value = slot_detail['value']['interpretedValue']
-                if value:
-                    labels.append(value)
+    logger.info(f"Lex response: {response}")
     
-    return labels
+    return get_labels_from_lex_response(response)
 
+def get_labels_from_lex_response(response):
+    extracted_labels = []
+    slots = response.get('sessionState', {}).get('intent', {}).get('slots', {})
+    for slot_detail in slots.values():
+        if slot_detail and slot_detail.get('value'):
+            value = slot_detail['value'].get('interpretedValue')
+            if value:
+                extracted_labels.append(value)
+    return extracted_labels
 
-def get_photo_path(labels):
-    img_paths = []
-    unique_labels = []
-    for x in labels:
-        if x not in unique_labels:
-            unique_labels.append(x)
-    labels = unique_labels
-    print("inside get photo path", labels)
-    for i in labels:
-        path = host + '/_search?q=labels:'+i
-        print(path)
-        response = requests.get(path, headers=headers,
-                                auth=("root", "Root@123"))
-        print("response from ES", response)
-        dict1 = json.loads(response.text)
-        hits_count = dict1['hits']['total']['value']
-        print("DICT : ", dict1)
-        for k in range(0, hits_count):
-            img_obj = dict1["hits"]["hits"]
-            img_bucket = dict1["hits"]["hits"][k]["_source"]["bucket"]
-            print("img_bucket", img_bucket)
-            img_name = dict1["hits"]["hits"][k]["_source"]["objectKey"]
-            print("img_name", img_name)
-            img_link = 'https://photosbucketb2cc.s3.us-west-2.amazonaws.com' + \
-                 '/' + str(img_name)
-            print(img_link)
-            img_paths.append(img_link)
-    print(img_paths)
-    return img_paths
+def fetch_image_paths_from_elasticsearch(labels):
+    unique_labels = list(set(labels))  # Removes duplicates
+    logger.info(f"Unique labels for image search: {unique_labels}")
+    return [image_url for label in unique_labels for image_url in get_image_urls_for_label(label)]
 
+def get_image_urls_for_label(label):
+    search_path = f"{ES_HOST}/_search?q=labels:{label}"
+    logger.info(f"Search path: {search_path}")
+    response = requests.get(search_path, headers=HEADERS, auth=("root", "Root@123"))
+    search_results = response.json()
+    logger.info(f"Response from Elasticsearch: {response}")
+    
+    return build_image_urls_from_search_results(search_results)
+
+def build_image_urls_from_search_results(search_results):
+    image_urls = []
+    for hit in search_results["hits"]["hits"]:
+        object_key = hit["_source"]["objectKey"]
+        image_url = f'https://photosbucketb2cc.s3.{REGION}.amazonaws.com/{object_key}'
+        logger.info(f"Image URL: {image_url}")
+        image_urls.append(image_url)
+    return image_urls
+
+def build_response(status_code, body):
+    return {
+        'statusCode': status_code,
+        'body': json.dumps(body),
+        'headers': {'Access-Control-Allow-Origin': '*'},
+        "isBase64Encoded": False
+    }
